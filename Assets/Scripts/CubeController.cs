85576475eadb54551779866ddc1f3ff0978f099f
skebis <keranen.emil@gmail.com>
2019-04-22 11:17:21 +0300

    VR osat

---
+++ Assets/Scripts/CubeController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+    private Camera pCamera = new Camera();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pCamera = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
